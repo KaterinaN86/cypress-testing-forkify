488124fb28a4d483df11457a4fb22e01860d4c03
Katerina Naumova <112091580+KaterinaN86@users.noreply.github.com>
2023-06-09 13:59:40 +0200

    Updated to enable parallel run with GitHub actions

---
+++ cypress/e2e/parallel-tests/testHeader.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+import header from "../pages/Header.js";
+
+describe("Test app header", () => {
+    //Cypress hook that refers to a function being called before executing test suite.
+    before(function () {
+        //Using Cypress fixture to load test data.
+        cy.fixture("nav-items").then(function (data) {
+            //Using globalThis to be able to access variable data from anywhere in the code, no mather the scope.
+            globalThis.data = data;
+        });
+    });
+    //Cypress hook used to call a function before each test is executed.
+    beforeEach(function () {
+        // runs before each test in the it block
+        cy.log("Open base URL");
+        //Explicitly set pageLoadTimeout
+        cy.visit("/", { timeout: 5000 });
+    });
+    it("Should verify main header container, app logo and menu elements.", () => {
+        header.verifyMainContainer();
+        header.verifyLogo();
+        header.verifyNavItems(data.navItem);
+    });
+    it("Should click random nav menu element.", () => {
+        header.clickRandomNavItemElement(data);
+    });
+});
---
+++ cypress/e2e/parallel-tests/testHomePage.js
@@ -0,0 +1,36 @@
+import homePage from "../pages/HomePage.js";
+
+/// <reference types="Cypress" />
+describe("Test home page", () => {
+    //Cypress hook that refers to a function being called before executing test suite.
+    before(function () {
+        cy.fixture("example").then(function (data) {
+            //Using globalThis to be able to access variable data from anywhere in the code, no mather the scope.
+            globalThis.data = data;
+        });
+    });
+    //Cypress hook used to call a function before each test is executed.
+    beforeEach(function () {
+        // runs before each test in the it block
+        cy.log("Open base URL");
+        cy.visit("/");
+    });
+    it("Should verify base URL, page title and document charset property.", () => {
+        cy.log("Verifying base URL");
+        //Accessing URL of the page that is currently active and verifying it using fixture.
+        cy.url().should("eq", data.URL);
+        cy.log("Verifying page title.");
+        //Accessing document.title property of the page that is currently active.
+        cy.title().should("include", data.pageTitle);
+        cy.log("Verifying document charset property");
+        //Accessing window document object and verifying charset property.
+        cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    });
+
+    it("Should verify main container is displayed", () => {
+        homePage.verifyMainContainerExists();
+    });
+    it("Should verify header is displayed", () => {
+        homePage.verifyHeaderExists();
+    });
+});
