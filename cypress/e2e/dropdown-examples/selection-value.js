375c8078403db9778b4f8ce35702ac6d2d48ccc2
Katerina Naumova <112091580+KaterinaN86@users.noreply.github.com>
2023-06-12 19:20:41 +0200

    Updated cy-split.yml

--- cypress/e2e/dropdown-examples/selection-value.js
+++
@@ -1,7 +0,0 @@
-it("Validate the dropdown option selection by it's value", () => {
-  cy.visit('https://www.bstackdemo.com/');
-  cy.get('select')
-    .select('lowestprice')
-    .invoke('val')
-    .should('eq', 'lowestprice');
-});
